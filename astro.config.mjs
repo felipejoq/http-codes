34b49ffcbe3e5de0745c24a530afebef01f2a60a
felipejoq <felipejofreq@gmail.com>
2026-03-29 18:21:18 -0300

    Update `vite` to v7.3.1 and modify site URL to `http.uncodigo.com` in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://codes.uncodigo.com',
+  site: 'https://http.uncodigo.com',
   prefetch: {
     prefetchAll: true
   }
